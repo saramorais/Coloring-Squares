b39428b52268c30239c85d5bdc38264b9d11e145
Sara Morais <saracristinasm@gmail.com>
2017-08-10 15:14:48 -0400

    final styles

--- src/components/colors.js
+++ src/components/colors.js
@@ -4,14 +4,14 @@ export default class Colors extends Component {
   render() {
     return (
       <div className="colors">
-        <p onClick={() => this.props.colorSelect("pink")} className="select select-pink" color="Pink">Pink</p>
-        <p onClick={() => this.props.colorSelect("red")} className="select select-red" color="Red">Red</p>
-        <p onClick={() => this.props.colorSelect("yellow")} className="select select-yellow" color="Yellow">Yellow</p>
-        <p onClick={() => this.props.colorSelect("blue")} className="select select-blue" color="Blue">Blue</p>
-        <p onClick={() => this.props.colorSelect("green")} className="select select-green" color="Green">Green</p>
-        <p onClick={() => this.props.colorSelect("brown")} className="select select-brown" color="Brown">Brown</p>
-        <p onClick={() => this.props.colorSelect("black")} className="select select-black" color="Black">Black</p>
-        <p onClick={() => this.props.colorSelect("white")} className="select" color="White">White</p>
+        <p onClick={() => this.props.colorSelect("pink")} className="select select-pink">Pink</p>
+        <p onClick={() => this.props.colorSelect("red")} className="select select-red">Red</p>
+        <p onClick={() => this.props.colorSelect("yellow")} className="select select-yellow">Yellow</p>
+        <p onClick={() => this.props.colorSelect("blue")} className="select select-blue">Blue</p>
+        <p onClick={() => this.props.colorSelect("green")} className="select select-green">Green</p>
+        <p onClick={() => this.props.colorSelect("brown")} className="select select-brown">Brown</p>
+        <p onClick={() => this.props.colorSelect("black")} className="select select-black">Black</p>
+        <p onClick={() => this.props.colorSelect("white")} className="select select-white">Eraser</p>
       </div>
     );
   }
